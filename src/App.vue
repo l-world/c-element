4a1baf224f89698e000a727fbd04225b21bcdcd9
Echo <heart0magic@163.com>
2021-03-12 17:59:04 +0800

    响应式实现和基于断点的类实现

--- src/App.vue
+++ src/App.vue
@@ -56,6 +56,11 @@
       <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
       <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
     </el-row>
+    <hr>
+    <h1>基与隐藏类的断点</h1>
+    <el-row :gutter="10">
+      <el-col class="hidden-xs-only" :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
   </div>
 </template>
 
--- src/components/element/Col.vue
+++ src/components/element/Col.vue
@@ -32,6 +32,7 @@ export default {
     md:[Number,Object],
     lg:[Number,Object],
     xl:[Number,Object],
+    tag:String
   },
   computed: {
     gutter() {
@@ -94,8 +95,11 @@ export default {
     right: $i / 24 * 100%;
   }
 }
-@media only screen and (max-width: 768px) {
+@media only screen and (max-width: 768px - 1) {
   @for $i from 0 through 24{
+    .el-col-xs-0{
+      display: none;
+    }
     .el-col-xs-#{$i}{
       width: $i / 24 * 100%;
     }
@@ -114,6 +118,9 @@ export default {
 }
 @media only screen and (min-width: 768px) {
   @for $i from 0 through 24{
+    .el-col-sm-0{
+      display: none;
+    }
     .el-col-sm-#{$i}{
       width: $i / 24 * 100%;
     }
@@ -133,6 +140,9 @@ export default {
 
 @media only screen and (min-width: 992px) {
   @for $i from 0 through 24{
+    .el-col-md-0{
+      display: none;
+    }
     .el-col-md-#{$i}{
       width: $i / 24 * 100%;
     }
@@ -150,27 +160,11 @@ export default {
   }
 }
 
-@media only screen and (min-width: 992px) {
-  @for $i from 0 through 24{
-    .el-col-md-#{$i}{
-      width: $i / 24 * 100%;
-    }
-    .el-col-offset-md-#{$i}{
-      margin-left: $i / 24 * 100%;
-    }
-    .el-col-push-md-#{$i}{
-      position: relative;
-      left: $i / 24 * 100%;
-    }
-    .el-col-pull-md-#{$i}{
-      position: relative;
-      right: $i / 24 * 100%;
-    }
-  }
-}
-
 @media only screen and (min-width: 1200px) {
   @for $i from 0 through 24{
+    .el-col-lg-0{
+      display: none;
+    }
     .el-col-lg-#{$i}{
       width: $i / 24 * 100%;
     }
@@ -190,6 +184,9 @@ export default {
 
 @media only screen and (min-width: 1920px) {
   @for $i from 0 through 24{
+    .el-col-xl-0{
+      display: none;
+    }
     .el-col-xl-#{$i}{
       width: $i / 24 * 100%;
     }
