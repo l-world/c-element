9f05e5c1c72d00531d854381d3034a13ddf77a05
Echo <heart0magic@163.com>
2021-03-12 10:12:43 +0800

    layout 响应式布局封装完成

--- src/components/element/Col.vue
+++ src/components/element/Col.vue
@@ -1,5 +1,5 @@
 <template>
-  <div 
+  <div
     :class="['el-col',classList]"
     :style="style"
   >
@@ -15,44 +15,60 @@ export default {
       type: Number,
       default: 24,
     },
-    offset:{
-      type:Number,
-      default:0
+    offset: {
+      type: Number,
+      default: 0,
     },
-    push:{
-      type:Number,
-      default:0
+    push: {
+      type: Number,
+      default: 0,
     },
-    pull:{
-      type:Number,
-      default:0
-    }
+    pull: {
+      type: Number,
+      default: 0,
+    },
+    xs:[Number,Object],
+    sm:[Number,Object],
+    md:[Number,Object],
+    lg:[Number,Object],
+    xl:[Number,Object],
   },
   computed: {
     gutter() {
       let parent = this.$parent;
-      while(parent && parent.$options.name !== "el-row"){
-        parent = parent.$parent
+      while (parent && parent.$options.name !== 'el-row') {
+        parent = parent.$parent;
       }
       return parent ? parent.gutter : 0;
     },
-    style(){
-      let style = {}
-      if(this.gutter){
-        style.paddingLeft = this.gutter / 2 + 'px';
-        style.paddingRight = style.paddingLeft
+    style() {
+      const style = {};
+      if (this.gutter) {
+        style.paddingLeft = `${this.gutter / 2}px`;
+        style.paddingRight = style.paddingLeft;
       }
-      return style
+      return style;
     },
-    classList(){
-      let classList = [];
-      ['span','offset','pull','push'].forEach((prop) => {
-        if(this[prop]){
-          classList.push( prop === 'span' ? `el-col-${prop}` : `el-col-${prop}-${this[prop]}` )
+    classList() {
+      const classList = [];
+      ['span', 'offset', 'pull', 'push'].forEach((prop) => {
+        if (this[prop]) {
+          classList.push(prop === 'span' ? `el-col-${this[prop]}` : `el-col-${prop}-${this[prop]}`);
+        }
+      });
+      ['xs','sm','md','lg','xl'].forEach( size => {
+        if(typeof this[size] === 'number'){
+          classList.push(`el-col-${size}-${this[size]}`)
+        }
+        else if(typeof this[size] === 'object'){
+          const props = this[size]
+          Object.keys(props).forEach( prop => {
+            classList.push(prop === 'span' ? `el-col-${size}-${props[prop]}` : `el-col-${size}-${prop}-${props[prop]}`)
+          })
         }
       })
       return classList;
-    }
+    },
   },
 };
 </script>
@@ -76,6 +92,118 @@ export default {
   .el-col-pull-#{$i}{
     position: relative;
     right: $i / 24 * 100%;
+  }
+}
+@media only screen and (max-width: 768px) {
+  @for $i from 0 through 24{
+    .el-col-xs-#{$i}{
+      width: $i / 24 * 100%;
+    }
+    .el-col-offset-xs-#{$i}{
+      margin-left: $i / 24 * 100%;
+    }
+    .el-col-push-xs-#{$i}{
+      position: relative;
+      left: $i / 24 * 100%;
+    }j
+    .el-col-pull-xs-#{$i}{
+      position: relative;
+      right: $i / 24 * 100%;
+    }
+  }
+}
+@media only screen and (min-width: 768px) {
+  @for $i from 0 through 24{
+    .el-col-sm-#{$i}{
+      width: $i / 24 * 100%;
+    }
+    .el-col-offset-sm-#{$i}{
+      margin-left: $i / 24 * 100%;
+    }
+    .el-col-push-sm-#{$i}{
+      position: relative;
+      left: $i / 24 * 100%;
+    }j
+    .el-col-pull-sm-#{$i}{
+      position: relative;
+      right: $i / 24 * 100%;
+    }
+  }
+}
+
+@media only screen and (min-width: 992px) {
+  @for $i from 0 through 24{
+    .el-col-md-#{$i}{
+      width: $i / 24 * 100%;
+    }
+    .el-col-offset-md-#{$i}{
+      margin-left: $i / 24 * 100%;
+    }
+    .el-col-push-md-#{$i}{
+      position: relative;
+      left: $i / 24 * 100%;
+    }j
+    .el-col-pull-md-#{$i}{
+      position: relative;
+      right: $i / 24 * 100%;
+    }
+  }
+}
+
+@media only screen and (min-width: 992px) {
+  @for $i from 0 through 24{
+    .el-col-md-#{$i}{
+      width: $i / 24 * 100%;
+    }
+    .el-col-offset-md-#{$i}{
+      margin-left: $i / 24 * 100%;
+    }
+    .el-col-push-md-#{$i}{
+      position: relative;
+      left: $i / 24 * 100%;
+    }
+    .el-col-pull-md-#{$i}{
+      position: relative;
+      right: $i / 24 * 100%;
+    }
+  }
+}
+
+@media only screen and (min-width: 1200px) {
+  @for $i from 0 through 24{
+    .el-col-lg-#{$i}{
+      width: $i / 24 * 100%;
+    }
+    .el-col-offset-lg-#{$i}{
+      margin-left: $i / 24 * 100%;
+    }
+    .el-col-push-lg-#{$i}{
+      position: relative;
+      left: $i / 24 * 100%;
+    }
+    .el-col-pull-lg-#{$i}{
+      position: relative;
+      right: $i / 24 * 100%;
+    }
+  }
+}
+
+@media only screen and (min-width: 1920px) {
+  @for $i from 0 through 24{
+    .el-col-xl-#{$i}{
+      width: $i / 24 * 100%;
+    }
+    .el-col-offset-xl-#{$i}{
+      margin-left: $i / 24 * 100%;
+    }
+    .el-col-push-xl-#{$i}{
+      position: relative;
+      left: $i / 24 * 100%;
+    }
+    .el-col-pull-xl-#{$i}{
+      position: relative;
+      right: $i / 24 * 100%;
+    }
   }
 }
 
--- src/components/element/Row.vue
+++ src/components/element/Row.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="el-row" :style="style">
+  <div 
+    :class="[
+      'el-row',
+      {'el-row--flex':this.type === 'flex'},
+      this.justify !== 'start' && `is-justify-${this.justify}`,
+      this.align !== 'top' && `is-align-${this.align}`
+    ]" 
+    :style="style"
+  >
     <slot></slot>
   </div>
 </template>
@@ -12,20 +20,70 @@ export default {
       type: Number,
       default: 0,
     },
-  },
-  computed:{
-    style(){
-      let style = {}
-      if(this.gutter){
-        style.marginLeft = -this.gutter / 2 + 'px'
-        style.marginRight = style.marginLeft
+    type:String,
+    justify:{
+      Number:String,
+      default:'start',
+      validator:val =>{
+        return ['start','end','center','space-around','space-between'].includes(val)
+
       }
-      return style
+    },
+    align:{
+      Number:String,
+      default:'top',
+      validator: val => {
+        return ['top','middle','bottom'].includes(val)
+      }
     }
-  }
+  },
+  computed: {
+    style() {
+      const style = {};
+      if (this.gutter) {
+        style.marginLeft = `${-this.gutter / 2}px`;
+        style.marginRight = style.marginLeft;
+      }
+      return style;
+    },
+  },
 };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.el-row{
+  &::after,
+  &::before{
+    display: block;
+    content: "";
+  }
+  &::after{
+     clear: both;
+  }
+  &--flex{
+    display: flex;
+    &::after,
+    &::before{
+      display: none;
+    }
+    &.is-justify-end{
+      justify-content: flex-end;
+    }
+    &.is-justify-center{
+      justify-content: center;
+    }
+    &.is-justify-space-around{
+      justify-content: space-around;
+    }
+    &.is-justify-space-between{
+      justify-content: space-between;
+    }
+    &.is-align-middle{
+      align-items:center
+    }
+    &.is-align-bottom{
+      align-items:flex-end
+    }
+  }
+}
 </style>
